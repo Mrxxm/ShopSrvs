7fadcbaddff9b7444e19c25b642cc6dec3beb71a
Mrxxm <362190221@qq.com>
2024-12-18 14:46:42 +0800

    通过手机号查询用户、通过id查询用户

--- user_srv/handler/user.go
+++ user_srv/handler/user.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
 	"shop_srvs/user_srv/global"
 	"shop_srvs/user_srv/model"
@@ -64,4 +66,43 @@ func (s *UserService) GetUserList(ctx context.Context, request *proto.PageInfo) 
 	}
 
 	return rsp, nil
+}
+
+func (s *UserService) GetUserByMobile(ctx context.Context, request *proto.MobileRequest) (*proto.UserInfoResponse, error) {
+
+	var user model.User
+
+	if request.Mobile == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "手机号码不能为空")
+	}
+	result := global.DB.Where("mobile = ?", request.Mobile).First(&user)
+	if result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "用户不存在")
+	}
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	userInfoResponse := ModelToResponse(user)
+
+	return &userInfoResponse, nil
+}
+
+func (s *UserService) GetUserById(ctx context.Context, request *proto.IdRequest) (*proto.UserInfoResponse, error) {
+	var user model.User
+
+	if request.Id == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "ID不能为空")
+	}
+	result := global.DB.Where("id = ?", request.Id).First(&user)
+	if result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "用户不存在")
+	}
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	userInfoResponse := ModelToResponse(user)
+
+	return &userInfoResponse, nil
 }
