a87eafa23126e9b7716ed629c6847585e6275b4b
Mrxxm <362190221@qq.com>
2025-02-05 13:59:57 +0800

    品牌 增删改

--- goods_srv/handler/brands.go
+++ goods_srv/handler/brands.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"shop_srvs/goods_srv/global"
 	"shop_srvs/goods_srv/model"
 	"shop_srvs/goods_srv/proto"
@@ -37,13 +40,42 @@ func (s *GoodsService) BrandList(ctx context.Context, req *proto.BrandFilterRequ
 	return &brandListResponse, nil
 }
 
-//
-//func (s *GoodsService) CreateBrand(context.Context, *proto.BrandRequest) (*proto.BrandInfoResponse, error) {
-//	return nil, nil
-//}
-//func (s *GoodsService) DeleteBrand(context.Context, *proto.BrandRequest) (*emptypb.Empty, error) {
-//	return nil, nil
-//}
-//func (s *GoodsService) UpdateBrand(context.Context, *proto.BrandRequest) (*emptypb.Empty, error) {
-//	return nil, nil
-//}
+func (s *GoodsService) CreateBrand(ctx context.Context, req *proto.BrandRequest) (*proto.BrandInfoResponse, error) {
+	//新建品牌
+	if result := global.DB.Where("name=?", req.Name).First(&model.Brands{}); result.RowsAffected == 1 {
+		return nil, status.Errorf(codes.InvalidArgument, "品牌已存在")
+	}
+
+	brand := &model.Brands{
+		Name: req.Name,
+		Logo: req.Logo,
+	}
+	global.DB.Save(brand)
+
+	return &proto.BrandInfoResponse{Id: brand.ID}, nil
+}
+
+func (s *GoodsService) DeleteBrand(ctx context.Context, req *proto.BrandRequest) (*emptypb.Empty, error) {
+	if result := global.DB.Delete(&model.Brands{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "品牌不存在")
+	}
+	return &emptypb.Empty{}, nil
+}
+
+func (s *GoodsService) UpdateBrand(ctx context.Context, req *proto.BrandRequest) (*emptypb.Empty, error) {
+	brands := model.Brands{}
+	if result := global.DB.First(&brands); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "品牌不存在")
+	}
+
+	if req.Name != "" {
+		brands.Name = req.Name
+	}
+	if req.Logo != "" {
+		brands.Logo = req.Logo
+	}
+
+	global.DB.Save(&brands)
+
+	return &emptypb.Empty{}, nil
+}
